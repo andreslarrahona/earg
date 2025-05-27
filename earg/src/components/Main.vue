8096ac1dc98426934d61167d7269502b1b50c1ab
Andrés Larrahona <andreslarrahona@gmail.com>
2025-05-27 00:46:32 -0300

    Commit inicial

---
+++ earg/src/components/Main.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  //msg: String,
+})
+
+</script>
+
+<template>
+
+  <body></body>
+  
+</template>
+
+<style lang="scss">
+@import "@/assets/styles/main.scss";
+
+</style>
