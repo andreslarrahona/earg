da03f69bb2745ae070baa8ea3ab5acc139419bb1
Andrés Larrahona <andreslarrahona@gmail.com>
2025-06-02 20:14:13 -0300

    Componente Geodesia.vue creado

--- earg/src/router/index.js
+++ earg/src/router/index.js
@@ -1,8 +1,11 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import Home from '@/views/Home.vue';
+import Geodesia from '@/views/Geodesia.vue';
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
+        {path: '/Geodesia', name: 'Geodesia', component: Geodesia},
+
 ]
 
 const router = createRouter({
---
+++ earg/src/views/Geodesia.vue
@@ -0,0 +1,29 @@
+<script setup>
+</script>
+<template>
+    <section>
+        <div>
+            <h1>Contexto</h1>
+            <p>Este programa se inició en 1984 con la participación en una campaña de mediciones Transit propuesta por la Facultad de Ciencias Astronómicas y Geofísicas de la 0LP (
+epto. de Astrometría). </p>
+<p>Desde entonces este programa a contribuido a vincular el territorio de la Isla Grande de Tierra del Fuego con redes
+geodésicas nacionales e internacionales. </p>
+
+
+        </div>
+        <img id="fagnano" src="@/assets/images/fagnano2.png">
+    </section>
+</template>
+<style scoped lang="scss">
+@import "@/assets/styles/main.scss";
+section{
+    background-color:$color-1;
+    div{
+        width:100%;
+        height:20vh;
+    }
+}
+#fagnano{
+    width:100%;
+}
+</style>
