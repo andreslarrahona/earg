c807a5af3902c24e886b7582f4ff8ae30c0d84a7
Andrés Larrahona <andreslarrahona@gmail.com>
2025-06-02 23:10:54 -0300

    Posgar07

--- earg/src/views/Geodesia.vue
+++ earg/src/views/Geodesia.vue
@@ -4,11 +4,34 @@
     <section>
         <div>
             <h1>Contexto</h1>
-            <p>Este programa se inició en 1984 con la participación en una campaña de mediciones Transit propuesta por la Facultad de Ciencias Astronómicas y Geofísicas de la 0LP (
-epto. de Astrometría). </p>
-<p>Desde entonces este programa a contribuido a vincular el territorio de la Isla Grande de Tierra del Fuego con redes
-geodésicas nacionales e internacionales. </p>
-
+            <div>
+            <p>
+                Este programa se inició en 1984 con la participación en una
+                campaña de mediciones Transit propuesta por la Facultad de 
+                Ciencias Astronómicas y Geofísicas de la UNLP (Depto. de Astrometría).
+            </p>
+            <p>
+                Desde entonces este programa ha contribuido a vincular el territorio 
+                de la Isla Grande de Tierra del Fuego con redes geodésicas nacionales e internacionales.
+            </p>
+            <p>
+                En 1993, conjuntamente con la entonces Dirección de Geodesia de la Provincia, dirigida por
+                el Agr. Carlos Zampatti, se diseñó y midió la Red Geodésica Provincial. 
+            </p>
+            <p>
+                Esta Red, cuya versión inicial se denominó <strong style="color:$color-2">TDF95</strong>,
+                constaba inicialmente de 23 pilares distribuidos en la Isla Grande de Tierra del Fuego.
+                Hoy, ampliada y mejorada, se estima que su error es menor a los 10 milímetros en cualquiera de las tres
+                coordenadas (Latitud, Longitud y Altura Elipsoidal).
+            </p>
+            <p>
+                TDF95 materializaba el marco de referencia POSGAR94 (marco Oficial de la Rep. Argentina desde 1997 hasta mayo 2009)
+                en la Isla Grande, hoy reemplazado por POSGAR 07 en Tierra del Fuego.
+            </p>
+            </div>
+            <div>
+                <img src="@/assets/images/logos/POSGAR07.png">
+            </div>
 
         </div>
         <img id="fagnano" src="@/assets/images/fagnano2.png">
@@ -20,7 +43,7 @@ section{
     background-color:$color-1;
     div{
         width:100%;
-        height:20vh;
+        height:80vh;
     }
 }
 #fagnano{
