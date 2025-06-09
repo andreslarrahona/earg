796580fe32ece4fef8c87f3aa2c19aa951cb97d8
Andrés Larrahona <andreslarrahona@gmail.com>
2025-06-08 21:47:09 -0300

    Sismologia creado

--- earg/src/router/index.js
+++ earg/src/router/index.js
@@ -3,12 +3,14 @@ import Home from '@/views/Home.vue';
 import Geodesia from '@/views/Geodesia.vue';
 import Meteoros from '@/views/Meteoros.vue';
 import Movimiento from '@/views/Movimiento.vue';
+import Sismologia from '@/views/Sismologia.vue';
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
     {path: '/Geodesia', name: 'Geodesia', component: Geodesia},
     {path: '/Meteoros', name: 'Meteoros', component: Meteoros},
     {path: '/Movimiento', name: 'Movimiento', component: Movimiento},
+    {path: '/Sismologia', name: 'Sismologia', component: Sismologia},
 
 ]
 
